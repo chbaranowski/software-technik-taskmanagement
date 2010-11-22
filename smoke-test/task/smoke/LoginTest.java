4dfe1d8ffe943a0b4cc6b69fdcd0b3e97f0a61bd
tux2323 <developer@noname>
2010-11-22 22:32:17 +0100

    Add UAT Tests to demo.

---
+++ smoke-test/task/smoke/LoginTest.java
@@ -0,0 +1,12 @@
+package task.smoke;
+
+import org.junit.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void testLogin() throws Exception {
+		
+	}
+
+}
---
+++ uat-test/task/uat/TaskTest.java
@@ -0,0 +1,43 @@
+package task.uat;
+
+import org.junit.Test;
+
+public class TaskTest {
+	
+	@Test
+	public void testLogin() throws Exception {
+		
+	}
+	
+	@Test
+	public void testLoginWithInvalidUsername() throws Exception {
+		
+	}
+	
+	@Test
+	public void testLoginWithInvalidPassword() throws Exception {
+		
+	}
+	
+	
+	@Test
+	public void testTaskOverview() throws Exception {
+	
+	}
+	
+	@Test
+	public void testCreateTask() throws Exception {
+
+	}
+	
+	@Test
+	public void testEditTask() throws Exception {
+	
+	}
+	
+	@Test
+	public void testDeleteTask() throws Exception {
+	
+	}
+	
+}
